ea13cc58063d57fd3f4d56044f38c1700db363fd
hamadikhalifa <ekidorevans719@gmail.com>
2025-03-15 19:09:46 +0300

    implement superbowlWn function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+
+
+  function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    console.log("Found win:", win); 
+    return win ? win.year : undefined;
+  }
