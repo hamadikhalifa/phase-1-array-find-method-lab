b20c6b08e8140940c3c9ee214f820c315243f2e8
hamadikhalifa <ekidorevans719@gmail.com>
2025-03-16 10:28:10 +0300

    implemented superbowlwin function using find()

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 function superbowlWin(record) {
-    const win = record.find(game => game.result === "W");
-    return win ? win.year : undefined;
-  }
+  const win = record.find(game => game.result === "W");
+  return win ? win.year : undefined;
+}
 
-
-  function superbowlWin(record) {
-    const win = record.find(game => game.result === "W");
-    console.log("Found win:", win); 
-    return win ? win.year : undefined;
-  }
+module.exports = superbowlWin;
